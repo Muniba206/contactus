724b9f66aeac6edcd726ac20408bd20a12374d58
Muniba206 <Muniba.Talha@yahoo.co.uk>
2017-04-11 22:10:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 // creating app
-var app = angular.module("contact",[]);
+(function(angular) {
+  
+  var app = angular.module("contact",[]);
 
 // creating form controller 
 
-app.controller("contactCtrl", function($scope){
+app.controller("contactCtrl", [$scope, $http, function($scope,$http){
+  
+    $scope.success = false;
+    $scope.error = false;
 
-  // validating phone number
+  $scope.sendMessage = function( input ) {
+      input.submit = true;
+      $http({
+          method: 'POST',
+          url: 'processForm.php',
+          data: angular.element.param(input),
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      })
+      .success( function(data) {
+        if ( data.success ) {
+          $scope.success = true;
+        } else {
+          $scope.error = true;
+        }
+      } );
+    }
+  }]);
+})(angular);
+
+  /* validating phone number
   $scope.nums = /^[1-9]{1}[0-9]{9}$/;
   $scope.submitForm = function(isValid) {
     alert("Form has been submitted")
 
   };
-});
+});*/
