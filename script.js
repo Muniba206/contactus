7ee4ecad9bd5cc9de4a805332b213ac06548f160
Muniba206 <Muniba.Talha@yahoo.co.uk>
2017-04-17 22:44:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ var app = angular.module("contact",[]);
 
 // creating form controller 
 app.controller("contactCtrl", function($scope){
-
-  // validating phone number
-  $scope.nums = /^[1-9]{1}[0-9]{7}$/;
  
   $scope.submitForm = function(isValid) {
   //  alert("Form has been submitted")
